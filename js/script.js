0adddf6bc7ef778a03a05f5103d5e846f41b2390
choiheejeong <h2eeee@naver.com>
2024-08-08 14:05:15 +0900

    240808 학습

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+//ScrollGiveClass 플러그인 호출
 var sgc1 = new ScrollGiveClass("header",{
     baseline:100
 });
+
+
+//Swiper플러그인 호출
+const swiper1 = new Swiper('#main_view', {
+    // Optional parameters
+    wrapperClass:'slider',     //클래스명 변경
+    slideClass:'view',
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
